13723dbb87b87bd347f7cbb4581f2d505cb9848b
aberkley <alexander.berkley@googlemail.com>
2014-08-28 15:42:15 +0100

    changed offline to false

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.9.4880.2" :scope "test"]
                  [clj-time "0.6.0"]]
-  :offline? true)
+  :offline? false)
